85954dda0385c0e8ec2b41dcd24d743c4d81696c
yattmanova <tanyayatmanova26@gmail.com>
2024-10-15 18:48:51 +0300

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -5,7 +5,7 @@ from restaurant_model import Restaurant
 restaurants = [
         Restaurant(
             id=1, 
-            title='тыккк',
+            title='тыкrrк',
             description='тык',
             photo_url='https://cs10.pikabu.ru/post_img/2019/02/08/4/1549601555162274027.jpg',
             address='тык тык'
